42db815352fd5db4d735d9c22f3a2e36ee641879
Thpffcj <1441732331@qq.com>
2017-10-21 10:20:09 +0800

    添加一个使用gradle构建SpringBoot的helloworld项目

---
+++ SpringBootBlog/HelloWorld/src/main/java/spring/boot/blog/Application.java
@@ -0,0 +1,15 @@
+package spring.boot.blog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by Thpffcj on 2017/10/21.
+ */
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}

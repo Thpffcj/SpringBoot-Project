b3816d89a471fd9a3d61e82eadfd0578d7d2e7da
Thpffcj <1441732331@qq.com>
2018-02-03 19:29:05 +0800

    添加简单的天气查询接口

---
+++ weather-project/src/main/java/cn/edu/nju/service/WeatherDataService.java
@@ -0,0 +1,26 @@
+package cn.edu.nju.service;
+
+import cn.edu.nju.vo.WeatherResponse;
+
+/**
+ * Created by Thpffcj on 2018/2/3.
+ */
+public interface WeatherDataService {
+
+    /**
+     * 根据城市ID查询天气数据
+     *
+     * @param cityId
+     * @return
+     */
+    WeatherResponse getDataByCityId(String cityId);
+
+    /**
+     * 根据城市名称查询天气数据
+     *
+     * @param cityName
+     * @return
+     */
+    WeatherResponse getDataByCityName(String cityName);
+
+}

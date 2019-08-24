00c98eb3d4b33ce3450a90f0cdf84116c17009a1
Thpffcj <1441732331@qq.com>
2019-08-24 20:44:01 +0800

    开一个广告系统的坑

---
+++ advertising-system/ad-eureka/src/main/java/cn/edu/nju/EurekaApplication.java
@@ -0,0 +1,18 @@
+package cn.edu.nju;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * Created by thpffcj on 2019/8/24.
+ */
+@EnableEurekaServer
+@SpringBootApplication
+public class EurekaApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(EurekaApplication.class, args);
+    }
+}

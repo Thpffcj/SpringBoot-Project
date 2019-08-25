3647e5972863c3fd101b7576b11f968f78cfac64
Thpffcj <1441732331@qq.com>
2019-08-25 08:28:14 +0800

    添加广告系统gateway

---
+++ advertising-system/ad-gateway/src/main/java/cn/edu/nju/ZuulGatewayApplication.java
@@ -0,0 +1,17 @@
+package cn.edu.nju;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.cloud.client.SpringCloudApplication;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+/**
+ * Created by thpffcj on 2019/8/25.
+ */
+@EnableZuulProxy
+@SpringCloudApplication
+public class ZuulGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZuulGatewayApplication.class, args);
+    }
+}
